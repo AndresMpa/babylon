eee5dda2bc6f99db7b4c643249e1324d25ad779e
AndresMpa <andresprieto2703@gmail.com>
2021-05-19 11:04:19 -0500

    Some fixes

--- practice/basics/events/keyboard/events.js
+++ practice/basics/events/keyboard/events.js
@@ -1,8 +1,6 @@
 let title = document.querySelector("h1");
-let paragraph = document.querySelector("p");
 
 document.addEventListener("keydown", function (event) {
   console.log(event.key);
-  alert('${event.key}');
-  p.innerHTML = `You presed ${event.key}`;
+  title.innerHTML = `You pressed ${event.key}`;
 });
