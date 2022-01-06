ccf8833c12ecd38913db0144ec2d45e44dda8a5f
AndresMpa <andresprieto2703@gmail.com>
2022-01-06 02:42:34 -0500

    Adding like 13  classes

---
+++ professionalJScourse/src/player.js
@@ -0,0 +1,26 @@
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+
+// Video tag
+const video = document.querySelector("video");
+// Buttons
+const playStop = document.querySelector("#playStop");
+const mute = document.querySelector("#mute");
+
+const player = new MediaPlayer({
+  element: video,
+  plugins: [new AutoPlay()],
+});
+
+playStop.onclick = () => {
+  playStop.innerHTML === `<i class="fas fa-pause"></i>`
+    ? (playStop.innerHTML = `<i class="fas fa-play"></i>`)
+    : (playStop.innerHTML = `<i class="fas fa-pause"></i>`);
+  player.playStop();
+};
+mute.onclick = () => {
+  mute.innerHTML === `<i class="fas fa-volume-up"></i>`
+    ? (mute.innerHTML = `<i class="fas fa-volume-mute"></i>`)
+    : (mute.innerHTML = `<i class="fas fa-volume-up"></i>`);
+  player.volumeMute();
+};
