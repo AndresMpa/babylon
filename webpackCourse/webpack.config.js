22de6cf7a3e0f31919c09c87d5dcfac767f48b02
Andres M. Prieto <andresprieto2703@gmail.com>
2022-02-24 11:18:37 -0500

    Copyng images from assents to dist dir

--- webpackCourse/webpack.config.js
+++ webpackCourse/webpack.config.js
@@ -1,5 +1,6 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -38,5 +39,13 @@ module.exports = {
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
+    })
   ],
 };
