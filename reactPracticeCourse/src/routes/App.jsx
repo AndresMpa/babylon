e779c1ff5b0a63cd5b0a4e4bdb374a9a3d8edf87
AndresMpa <andresprieto2703@gmail.com>
2022-06-23 20:17:15 -0500

    Huge change on React project

---
+++ reactPracticeCourse/src/routes/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../containers/Layout";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import PasswordRecovery from "../pages/PasswordRecovery";
+import SendEmail from "../pages/SendEmail";
+import NewPassword from "../pages/NewPassword";
+import MyAccount from "../pages/MyAccount";
+import CreateAccount from "../pages/CreateAccount";
+import Checkout from "../pages/Checkout";
+import Orders from "../pages/Orders";
+import NotFound from "../pages/NotFound";
+
+import "../styles/global.scss";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route
+            exact
+            path="/password-recovery"
+            element={<PasswordRecovery />}
+          />
+          <Route exact path="/send-email" element={<SendEmail />} />
+          <Route exact path="/new-password" element={<NewPassword />} />
+          <Route exact path="/account" element={<MyAccount />} />
+          <Route exact path="/signup" element={<CreateAccount />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route exact path="/orders" element={<Orders />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default App;
