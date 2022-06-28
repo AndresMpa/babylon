4cd5dd78e98109d0d6c4453b9f4436fabdadb07f
AndresMpa <andresprieto2703@gmail.com>
2022-06-28 04:47:07 -0500

    Checkout
    Fixing checkout component

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import AppContext from '@context/AppContext';
+
+import Head from 'next/head';
 
 import OrderItem from '@components/OrderItem';
 
+import { getDate, sumTotal } from '@util/cart';
+
 import styles from '@styles/Checkout.module.scss';
 
+
 const Checkout = () => {
-	return (
-		<div className={styles.Checkout}>
-			<div className={styles["Checkout-container"]}>
-				<h1 className={styles.title}>My order</h1>
-				<div className={styles["Checkout-content"]}>
-					<div className={styles.order}>
-						<p>
-							<span>03.25.21</span>
-							<span>6 articles</span>
-						</p>
-						<p>$560.00</p>
-					</div>
-				</div>
-				<OrderItem />
-			</div>
-		</div>
-	);
-}
+  const { state } = useContext(AppContext);
+  return (
+    <>
+      <Head>
+        <title>Checkout</title>
+      </Head>
+      <div className={styles.Checkout}>
+        <div className={styles['Checkout-container']}>
+          <h1 className={styles.title}>My order</h1>
+          <div className={styles['Checkout-content']}>
+            <div className={styles.order}>
+              <p>
+                <span>{ getDate() }</span>
+                <span>Item on cart: { state.cart.length }</span>
+              </p>
+              <p>Total: $ {sumTotal(state.cart)}</p>
+            </div>
+          </div>
+          {state.cart.map((product) => (
+            <OrderItem product={product} key={`orderItem-${product.id}`} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Checkout;
