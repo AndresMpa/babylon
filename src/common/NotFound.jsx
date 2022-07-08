d4aafb83ecf3d258b3a26c9b916ad36dff4b7349
AndresMpa <andresprieto2703@gmail.com>
2022-07-08 03:48:43 -0500

    Not found component
    Creating a not found component

---
+++ src/common/NotFound.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="py-16 px-8 bg-white flex justify-center align-middle">
+      <div class="space-y-0.5 text-center">
+        <h1 class="text-2xl text-black font-semibold">404</h1>
+        <p class="text-slate-500 font-medium">Page not found</p>
+      </div>
+    </div>
+  );
+}
