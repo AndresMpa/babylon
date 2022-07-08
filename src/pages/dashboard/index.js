f0dfca9f2e945a1b88823050281d8a0d419d0383
AndresMpa <andresprieto2703@gmail.com>
2022-07-07 23:56:16 -0500

    Charts
    Using chats on main dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -3,6 +3,7 @@ import useFetch from '@hooks/useFetch';
 import endPoints from '@services/api';
 
 import Pagination from '@common/Pagination';
+import Chart from '@common/Char';
 
 const LIMIT = 8;
 const PRODUCT_LIMIT = 0;
@@ -12,16 +13,43 @@ export default function Dashboard() {
   const products = useFetch(
     endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET)
   );
-
-  let paginator = usePaginator();
+  
+  const paginator = usePaginator();
 
   const productsList = products.slice(
     paginator.current - 1,
     paginator.current + 8
   );
 
+  const categoryNames = productsList?.map((product) => product.category);
+  const categoryCount = categoryNames?.map((category) => category.name);
+
+  const countOccurrences = (arr) =>
+    arr.reduce(
+      (prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev),
+      {}
+    );
+
+  const data = {
+    datasets: [
+      {
+        borderWidth: 2,
+        label: 'Categories',
+        data: countOccurrences(categoryCount),
+        backgroundColor: [
+          '#ffbb11',
+          '#c0c0c0',
+          '#50Af95',
+          '#f0ba2f',
+          '#2a71d0',
+        ],
+      },
+    ],
+  };
+
   return (
     <>
+      <Chart className="mb-8 mt-2" chartData={data}></Chart>
       <Pagination elements={products} limit={LIMIT} />
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
