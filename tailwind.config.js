3bd8b024cdfad81e8c004f968d42d149017b1776
AndresMpa <andresprieto2703@gmail.com>
2022-07-08 05:23:57 -0500

    Quick fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 const colors = require('tailwindcss/colors');
 
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
