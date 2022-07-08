5942dd289515c25355b226f22be46fa5f139ece9
AndresMpa <andresprieto2703@gmail.com>
2022-07-08 02:24:22 -0500

    Quick fix

--- src/common/Alert.jsx
+++ src/common/Alert.jsx
@@ -7,11 +7,13 @@ const Alert = ({ alert, handleClose }) => {
     }, 9000);
   }
 
+  const color = alert?.type === 'success' ? 'green-100' : 'red-100';
+
   return (
     <>
       {alert?.active && (
-        <div x-data className="bg-indigo-100 p-5 w-full rounded mb-8">
-          <div className="flex space-x-3">
+        <div x-data className="bg-white p-5 w-full rounded mb-8">
+          <div className={`bg-${color} flex space-x-3`}>
             <div className="flex-1 leading-tight text-sm text-black font-medium">
               {alert.message}
             </div>
