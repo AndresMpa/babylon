f5ba356484d097ac7a8d8f581aa263438b68d6b0
AndresMpa <andresprieto2703@gmail.com>
2022-07-15 03:31:09 -0500

    N:N schema
    Handling Order - Product relation from Order schema

--- schemas/order.schema.js
+++ schemas/order.schema.js
@@ -1,7 +1,10 @@
 const Joi = require('joi');
 
 const id = Joi.number().integer();
+const orderId = Joi.number().integer();
+const productId = Joi.number().integer();
 const customerId = Joi.number().integer();
+const amount = Joi.number().integer().min(1);
 
 const getOrderSchema = Joi.object({
   id: id.required(),
@@ -11,7 +14,10 @@ const createOrderSchema = Joi.object({
   customerId: customerId.required(),
 });
 
-module.exports = {
-  getOrderSchema,
-  createOrderSchema,
-};
+const addItemSchema = Joi.object({
+  productId: productId.required(),
+  orderId: orderId.required(),
+  amount: amount.required(),
+});
+
+module.exports = { getOrderSchema, createOrderSchema, addItemSchema };
