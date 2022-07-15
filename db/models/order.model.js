0920b39833181ea64da8d29ff8cd1589dc81946b
AndresMpa <andresprieto2703@gmail.com>
2022-07-15 03:33:38 -0500

    Virtual field
    Using a virtual field to get totals from orders price

--- db/models/order.model.js
+++ db/models/order.model.js
@@ -28,12 +28,29 @@ const OrderSchema = {
     field: 'created_at',
     defaultValue: Sequelize.NOW,
   },
+  total: {
+    type: DataTypes.VIRTUAL,
+    get() {
+      if (this.items.length > 0) {
+        return this.items.reduce((total, item) => {
+          return total + item.price * item.OrderProduct.amount;
+        }, 0);
+      }
+      return 0
+    },
+  },
 };
 
 class Order extends Model {
   static associate(models) {
     this.belongsTo(models.Customer, {
       as: 'customer',
+    });
+    this.belongsToMany(models.Product, {
+      as: 'items',
+      through: models.OrderProduct,
+      foreignKey: 'orderId',
+      otherKey: 'productId',
     });
   }
 
