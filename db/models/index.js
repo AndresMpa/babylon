54cfe7e9bf05f55630dc9647dbaa10553ea3af19
AndresMpa <andresprieto2703@gmail.com>
2022-07-15 03:30:31 -0500

    OrderProduct
    Setting up many to many relation

--- db/models/index.js
+++ db/models/index.js
@@ -1,3 +1,4 @@
+const { OrderProduct, OrderProductSchema } = require('./order-products.model');
 const { Customer, CustomerSchema } = require('./customer.model');
 const { Category, CategorySchema } = require('./category.model');
 const { Product, ProductSchema } = require('./product.model');
@@ -5,12 +6,14 @@ const { Order, OrderSchema } = require('./order.model');
 const { User, UserSchema } = require('./user.model');
 
 function setupModels(sequelize) {
+  OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
   Order.init(OrderSchema, Order.config(sequelize));
   User.init(UserSchema, User.config(sequelize));
 
+  OrderProduct.associate(sequelize.models);
   Customer.associate(sequelize.models);
   Category.associate(sequelize.models);
   Product.associate(sequelize.models);
