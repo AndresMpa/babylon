2012f32ec59b855371f777ee763eb143d0782d56
AndresMpa <andresprieto2703@gmail.com>
2022-07-14 20:32:13 -0500

    Role
    Using role

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -8,12 +8,12 @@ const role = Joi.string().min(5);
 const createUserSchema = Joi.object({
   email: email.required(),
   password: password.required(),
-  //role: role.required(),
+  role: role.required(),
 });
 
 const updateUserSchema = Joi.object({
   email: email,
-  //role: role,
+  role: role,
 });
 
 const getUserSchema = Joi.object({
