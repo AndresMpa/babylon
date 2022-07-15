7d6e79b05e9accf457093af8699410e45ce4e26a
AndresMpa <andresprieto2703@gmail.com>
2022-07-14 20:30:39 -0500

    Postgres
    Switching back to postgres

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,10 +5,10 @@ const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: console.log,
 });
 
