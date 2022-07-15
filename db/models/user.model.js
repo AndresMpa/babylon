99a811bbbb4643844e4bfb4d9705669553bed08c
AndresMpa <andresprieto2703@gmail.com>
2022-07-14 20:29:48 -0500

    Addinf role schema

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -14,6 +14,11 @@ const UserSchema = {
     allowNull: false,
     type: DataTypes.STRING,
   },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'customer'
+  },
   password: {
     allowNull: false,
     type: DataTypes.STRING,
