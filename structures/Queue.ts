3510ee1a96102cb06d3d8767fd9785bce680b0c9
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-21 15:55:36 -0500

    Queue
    Adding Queue data structure

---
+++ structures/Queue.ts
@@ -0,0 +1,43 @@
+import Node from "./Node";
+
+// First In First Out
+class Queue {
+  last: any;
+  first: any;
+  length: number;
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first;
+  }
+  enqueue(value: any) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+
+    return newNode;
+  }
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+
+    return this;
+  }
+}
+
+export default Queue;
