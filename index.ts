4287e20208fca08b104144cee7d9c9fd909db926
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-21 13:42:19 -0500

    Adding singly linked list structure

--- index.ts
+++ index.ts
@@ -1,17 +1,1 @@
-import HashTable from "./structures/HashTable";
 
-const hashTable: HashTable = new HashTable(50);
-
-hashTable.set("AndresMpa", 20);
-hashTable.set("JennDev", 21);
-hashTable.set("DK8", 14);
-hashTable.set("Mappa", 22);
-hashTable.set("KuffSnack", 10);
-hashTable.set("Riukendo", 40);
-hashTable.set("Sun", 23);
-
-console.log(hashTable.get("AndresMpa"));
-
-console.log(hashTable.deleteByKey("AndresMpa"));
-
-console.log(hashTable.get("AndresMpa"));
---
+++ structures/SinglyLinkedList.ts
@@ -0,0 +1,100 @@
+class Node {
+  next: any;
+  value: any;
+  constructor(value: any) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  head: any;
+  tail: any;
+  length: number;
+  constructor(value: any) {
+    this.head = new Node(value);
+    this.tail = this.head;
+
+    this.length = 1;
+  }
+
+  private getIndex(index: number) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return currentNode;
+  }
+
+  append(value: any) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
+    return newNode;
+  }
+  prepend(value: any) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+
+    return newNode;
+  }
+  insert(index: number, value: any) {
+    if (index >= this.length) {
+      console.warn(
+        `${index} exceeded list length (${this.length}); appding your value at the end`
+      );
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const previousPointer = this.getIndex(index);
+    const holdingPointer = previousPointer.next;
+    previousPointer.next = newNode;
+    newNode.next = holdingPointer;
+
+    this.length++;
+
+    return newNode;
+  }
+  delete(index: number) {
+    if (index >= this.length) {
+      throw new Error(`${index} exceeded list length (${this.length})`);
+    }
+    const previousPointer = this.getIndex(index);
+    const deletedNode = previousPointer.next;
+    const nextNode = deletedNode.next;
+    previousPointer.next = nextNode;
+
+    return deletedNode;
+  }
+  search(index: number) {
+    if (index >= this.length - 1) {
+      throw new Error(`${index} exceeded search threshold`);
+    }
+
+    const previousPointer = this.getIndex(index);
+    const foundPointer = previousPointer.next;
+    const LastPointer = foundPointer.next;
+
+    return [previousPointer, foundPointer, LastPointer];
+  }
+  values() {
+    let currentPointer = this.head;
+    const values = [];
+    while (currentPointer !== null) {
+      values.push(currentPointer.value);
+      currentPointer = currentPointer.next;
+    }
+    return values;
+  }
+}
+
+export default SinglyLinkedList;
