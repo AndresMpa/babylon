4e6f8d09c81b0d4fe039b8fdc3025834685f4379
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-22 00:36:07 -0500

    Fix
    I didn't need the nodes duh

--- test/singlyLinkedList.test.js
+++ test/singlyLinkedList.test.js
@@ -1,50 +1,49 @@
 import SinglyLinkedList from "../structures/SinglyLinkedList";
-import Node from "../structures/SinglyLinkedList";
 
 const setUp = () => {
-  const firstNode = new Node(81);
+  const firstNode = 81;
 
   const linkedList = new SinglyLinkedList(firstNode);
 
-  linkedList.append(new Node(82));
-  linkedList.append(new Node(83));
-  linkedList.append(new Node(84));
-  linkedList.append(new Node(85));
-  linkedList.append(new Node(86));
-  linkedList.append(new Node(87));
-  linkedList.append(new Node(88));
-  linkedList.append(new Node(89));
+  linkedList.append(82);
+  linkedList.append(83);
+  linkedList.append(84);
+  linkedList.append(85);
+  linkedList.append(86);
+  linkedList.append(87);
+  linkedList.append(88);
+  linkedList.append(89);
 
   return linkedList;
 };
 
 test("Append data to linked list", () => {
   const linkedList = setUp();
-  const data = linkedList.append(new Node(90));
-  expect(data.value.head.value).toBe(90);
+  const data = linkedList.append(90);
+  expect(data.value).toBe(90);
 });
 test("Prepend data to linked list", () => {
   const linkedList = setUp();
-  const data = linkedList.prepend(new Node(90));
-  expect(data.value.head.value).toBe(90);
+  const data = linkedList.prepend(90);
+  expect(data.value).toBe(90);
 });
 test("Insert data to linked list", () => {
   const linkedList = setUp();
-  const data = linkedList.insert(4, new Node(90));
-  expect(data.value.head.value).toBe(90);
+  const data = linkedList.insert(4, 90);
+  expect(data.value).toBe(90);
 });
 test("Delete a fourth element from linked list", () => {
   const linkedList = setUp();
   const data = linkedList.delete(4);
-  expect(data.value.head.value).toBe(86);
+  expect(data.value).toBe(86);
 });
 test("Get data around a node", () => {
   const linkedList = setUp();
-  const data = linkedList.search(6).map((item) => item.value.head.value);
+  const data = linkedList.search(6).map((item) => item.value);
   expect(data).toStrictEqual([87, 88, 89]);
 });
 test("Get all values", () => {
   const linkedList = setUp();
-  const data = linkedList.values().map((item) => item.head.value);
+  const data = linkedList.values().map((item) => item);
   expect(data).toStrictEqual([81, 82, 83, 84, 85, 86, 87, 88, 89]);
 });
