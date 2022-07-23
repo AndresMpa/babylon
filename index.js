7bda020e7617c8b6e2448256fda8da8000ca557a
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-23 00:34:07 -0500

    Node as monolith
    Using node to show some basic HTML data

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
 
-router.get('/', (req, res) => {
+router.get('/test', (req, res) => {
   if (req.query.error === 'ok') {
     error(req, res, 'Testing error', 400);
   } else {
     success(req, res, 'Testing success', 200);
   }
 });
+
+app.use('/', express.static('./public'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
