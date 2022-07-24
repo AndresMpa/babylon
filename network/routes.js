ec818e904beee024b06f455e223924a704d1f54e
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-24 07:38:17 -0500

    Adding chat path

--- network/routes.js
+++ network/routes.js
@@ -1,13 +1,15 @@
 const messageComponent = require('../components/message/network');
+const chatComponent = require('../components/chat/network');
 const userComponent = require('../components/user/network');
 
 const router = require('express').Router();
 
 function endpoint(app) {
-  app.use(router)
+  app.use(router);
 
   // Router paths
   router.use('/message', messageComponent);
+  router.use('/chat', chatComponent);
   router.use('/user', userComponent);
 }
 
