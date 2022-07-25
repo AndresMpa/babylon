7e2a1dd8be56100151e10397436e1c5309788ae7
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-25 06:46:07 -0500

    Using default status

--- network/response.handler.js
+++ network/response.handler.js
@@ -1,12 +1,22 @@
-function success(req, res, message, status) {
+const statusMessage = {
+  200: 'Done',
+  201: 'Created',
+  400: 'Invalid format',
+  500: 'Internal error',
+};
+
+function success(req, res, data, status = 200) {
   console.log(`[Request]: ${message}`);
+  const message = data || statusMessage[status];
+
   res.status(status).send({
     response: message,
   });
 }
 
-function error(req, res, message, status) {
+function error(req, res, data, status = 500) {
   console.error(`[ERROR: response.error]: ${message}`);
+  const message = data || statusMessage[status];
   res.status(status).send({
     error: message,
   });
