5a51bb016a30b07ac13913b70aaf97869bc52c8f
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-25 06:46:31 -0500

    Using cors to handle CORS

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const socket = require('./config/socket');
 const { config } = require('./config');
 const connect = require('./config/db');
 const express = require('express');
+const cors = require('cors');
 
 //Express instance
 const app = express();
@@ -13,6 +14,7 @@ const server = require('http').Server(app);
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 // MongoBD connection
 connect();
