e158da08f5d55aa30c1d93bff15b1cc3e8053dae
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-25 06:46:59 -0500

    Some others env environments

--- config/index.js
+++ config/index.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'development',
+
   port: process.env.PORT || 3000,
+
+  appHost: process.env.APP_HOST,
+  appRoute: process.env.APP_ROUTE,
+  appFiles: process.env.APP_FILES,
+
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
