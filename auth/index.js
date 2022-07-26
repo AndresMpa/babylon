6d59ea368dc24460b51d31ca031ea119223a7bcf
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-26 12:02:06 -0500

    Auth
    Extending functionalities

--- auth/index.js
+++ auth/index.js
@@ -1,12 +1,41 @@
+const config = require("../config");
 const jwt = require("jsonwebtoken");
 
 function sign(data) {
-  return jwt.sign(data, "secretOrPrivateKey");
+  return jwt.sign(data, config.jwt.secret);
 }
 
 function validate(token) {
-  return jwt.verify(token, "secretOrPublicKey");
+  return jwt.verify(token, config.jwt.secret);
 }
+
+function getToken(auth) {
+  if (!auth) {
+    throw new Error("There's no token");
+  }
+
+  if (!auth.indexOf("Bearer ")) {
+    throw new Error("Invalid format");
+  }
+
+  let token = auth.replace("Bearer ", "");
+  return token;
+}
+
+function decodeHeader(request) {
+  const authorization = request.headers.authorization || "";
+  const token = getToken(authorization);
+  const decoded = validate(token);
+
+  req.user = decoded;
+}
+
+const check = {
+  own: (req, owner) => {
+    const decoded = decodeHeader(req);
+    console.log(decoded);
+  },
+};
 
 module.exports = {
   validate,
