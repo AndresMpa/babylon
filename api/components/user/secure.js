3884d910ab1d7c935692de66f2c7bff62c6b677d
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-26 12:02:31 -0500

    Middleware
    Using a middleware to handle auth

---
+++ api/components/user/secure.js
@@ -0,0 +1,21 @@
+const auth = require("../../../auth");
+
+function checkAuth(action) {
+  function middleware(req, res, next) {
+    switch (action) {
+      case "update": {
+        const owner = req.body.id;
+        auth.check.own(req, owner);
+        break;
+      }
+      default: {
+        next();
+      }
+    }
+  }
+  return middleware;
+}
+
+module.exports = {
+  checkAuth,
+};
