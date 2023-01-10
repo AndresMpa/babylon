75469385d3ef8463b247ffd135db739549ab0961
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-10 02:20:06 -0500

    Adding mongo adapter

--- config/datastores.js
+++ config/datastores.js
@@ -51,6 +51,8 @@ module.exports.datastores = {
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
 
+    adapter: 'sails-mongo',
+    url: 'mongodb://root:1234@localhost:27017/admin',
   },
 
 
