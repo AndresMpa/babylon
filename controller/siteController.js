4f603eb1ec13742af3852769699dd9f0d8f84d79
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-18 08:11:44 -0500

    User state
    Adding general required state "user"

--- controller/siteController.js
+++ controller/siteController.js
@@ -1,18 +1,21 @@
 function home(req, h) {
   return h.view("index", {
     title: "Home",
+    user: req.state.user,
   });
 }
 
 function register(req, h) {
   return h.view("register", {
     title: "Register",
+    user: req.state.user,
   });
 }
 
 function login(req, h) {
   return h.view("login", {
     title: "Login",
+    user: req.state.user,
   });
 }
 
