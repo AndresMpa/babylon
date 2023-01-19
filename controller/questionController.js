3e02a26850e3ac37f42972705b9e9dfbc3575028
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-19 17:46:36 -0500

    Integration and refactor
    Refactoring a little question controller to handle with user state,
    protecting routes, also integrating server method to question controller

--- controller/questionController.js
+++ controller/questionController.js
@@ -1,6 +1,10 @@
 const { questions } = require("../models");
 
 async function createQuestion(req, h) {
+  if (!req.state.user) {
+    return h.redirect("/login");
+  }
+
   try {
     await questions.create(req.payload, req.state.user);
     return h.view("ask", {
@@ -24,17 +28,39 @@ async function createQuestion(req, h) {
 }
 
 async function answerQuestion(req, h) {
+  if (!req.state.user) {
+    return h.redirect("/login");
+  }
+
   try {
     await questions.answer(req.payload, req.state.user);
     console.log("Answer created", req.payload.id);
+
+    return h.redirect(`/question/${req.payload.id}`);
   } catch (error) {
     console.error(error);
   }
+}
 
-  return h.redirect(`/question/${req.payload.id}`);
+async function setRightAnswer(req, h) {
+  if (!req.state.user) {
+    return h.redirect("/login");
+  }
+  try {
+    const result = await req.server.methods.setRightAnswer(
+      req.params.questionId,
+      req.params.answerId,
+      req.state.user
+    );
+
+    return h.redirect(`/question/${req.params.questionId}`);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
   createQuestion: createQuestion,
   answerQuestion: answerQuestion,
+  setRightAnswer: setRightAnswer,
 };
