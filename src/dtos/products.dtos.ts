6abf6989cb4c83d2ad8a0b0d2804a85c74f8f913
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-25 16:20:43 -0500

    Real validation schemes
    Extending DTOs to handle with validation schemes

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,8 +1,24 @@
+import { IsNotEmpty, IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly price: number;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly stock: number;
+
+  @IsNotEmpty()
+  @IsUrl()
   readonly image: string;
 }
 
