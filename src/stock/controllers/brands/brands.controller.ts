daa8cfff900739dabf45cb8ea743aa1c21d35caa
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-26 16:17:25 -0500

    Brands update
    Adding some features to Brand controller using Brand service

--- src/stock/controllers/brands/brands.controller.ts
+++ src/stock/controllers/brands/brands.controller.ts
@@ -1,18 +1,63 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Put,
+  Get,
+  Post,
+  Body,
+  Param,
+  Query,
+  Controller,
+  ParseIntPipe,
+  Delete,
+} from '@nestjs/common';
+
+import { BrandsService } from '../../services/brands/brands.service';
+
+import { CreateBrandsDto, UpdateBrandsDto } from 'src/stock/dtos/brands.dtos';
 
 @Controller('brands')
 export class BrandsController {
+  constructor(private brandService: BrandsService) {}
+
   @Get()
   getAll(@Query('limit') limit = 1, @Query('offset') offset = 0) {
     return {
       message: `Brands limit: ${limit} offset ${offset}`,
+      data: this.brandService.findAll(),
     };
   }
 
   @Get(':brandId')
-  getDetails(@Param('brandId') brandId: string) {
+  getDetails(@Param('brandId', ParseIntPipe) brandId: number) {
     return {
       message: `Brand id ${brandId}`,
+      data: this.brandService.findOne(brandId),
+    };
+  }
+
+  @Post()
+  create(@Body() payload: CreateBrandsDto) {
+    return {
+      message: 'Data created',
+      data: this.brandService.create(payload),
+    };
+  }
+
+  @Put(':brandId')
+  update(
+    @Param('brandId', ParseIntPipe) brandId: number,
+    @Body() payload: UpdateBrandsDto,
+  ) {
+    return {
+      message: `Brand ${brandId} created`,
+      data: this.brandService.update(brandId, payload),
+    };
+  }
+
+  @Delete(':brandId')
+  remove(@Param('brandId', ParseIntPipe) brandId: number) {
+    return {
+      message: `Brand ${brandId} deleted`,
+      data: this.brandService.remove(brandId),
     };
   }
 }
--- src/stock/services/brands/brands.service.ts
+++ src/stock/services/brands/brands.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { Brand } from 'src/stock/entities/brand.entity';
+
+import { CreateBrandsDto, UpdateBrandsDto } from 'src/stock/dtos/brands.dtos';
 
 @Injectable()
-export class BrandsService {}
+export class BrandsService {
+  private counterIdentifier = 1;
+  private brands: Brand[] = [
+    {
+      identifier: 1,
+      name: 'Example brand',
+      image: 'https://some-data.com',
+    },
+  ];
+
+  findAll() {
+    return this.brands;
+  }
+
+  findOne(index: number) {
+    const brand = this.brands.find((item) => item.identifier === index);
+    if (!brand) {
+      throw new NotFoundException(
+        `There's no a brand assigned to ${index} identifier`,
+      );
+    } else {
+      return brand;
+    }
+  }
+
+  create(payload: CreateBrandsDto) {
+    this.counterIdentifier += 1;
+    const newCategory = {
+      identifier: this.counterIdentifier,
+      ...payload,
+    };
+
+    this.brands.push(newCategory);
+
+    return newCategory;
+  }
+
+  update(identifier: number, payload: UpdateBrandsDto) {
+    const brand = this.findOne(identifier);
+
+    if (brand) {
+      const index = this.brands.findIndex(
+        (item) => item.identifier === identifier,
+      );
+      this.brands[index] = {
+        ...brand,
+        ...payload,
+      };
+      return this.brands[index];
+    }
+  }
+
+  remove(identifier: number) {
+    const index = this.brands.findIndex(
+      (item) => item.identifier === identifier,
+    );
+    if (index === -1) {
+      throw new NotFoundException(
+        `There's no brands assigned to ${index} identifier`,
+      );
+    }
+    this.brands.splice(index, 1);
+    return true;
+  }
+}
