81fbff41c39815639c7aa179adab3ce10e23c5b9
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-26 12:53:56 -0500

    Big refactor
    Moving from Single module app to modular orientation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,9 @@
 import { Module } from '@nestjs/common';
 
-// Controllers
-import { AppController } from './app.controller';
-import { ProductsController } from './controllers/products/products.controller';
-import { CatergoriesController } from './controllers/categories/categories.controller';
-import { OrdersController } from './controllers/orders/orders.controller';
-import { UsersController } from './controllers/users/users.controller';
-import { BrandsController } from './controllers/brands/brands.controller';
-import { CustomersController } from './controllers/customers/customers.controller';
-
-// Services
-import { AppService } from './app.service';
-import { ProductsService } from './services/products/products.service';
+import { StockModule } from './stock/stock.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    ProductsController,
-    CatergoriesController,
-    OrdersController,
-    UsersController,
-    BrandsController,
-    CustomersController,
-  ],
-  providers: [AppService, ProductsService],
+  imports: [StockModule, UsersModule],
 })
 export class AppModule {}
