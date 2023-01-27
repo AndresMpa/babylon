9f3e5133fcdd534dd386ef364602e8e41074ee2e
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-26 22:59:28 -0500

    Admins
    Adding admins controller and service

--- src/users/controllers/admins/admins.controller.ts
+++ src/users/controllers/admins/admins.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Get,
+  Put,
+  Post,
+  Delete,
+  Body,
+  Param,
+  Controller,
+  ParseIntPipe,
+} from '@nestjs/common';
+
+import { AdminsService } from 'src/users/services/admins/admins.service';
+
+import { CreateAdminDto, UpdateAdminDto } from 'src/users/dtos/admins.dtos';
 
 @Controller('admins')
-export class AdminsController {}
+export class AdminsController {
+  constructor(private adminService: AdminsService) {}
+  @Get(':adminId')
+  getDetails(@Param('adminId', ParseIntPipe) adminId: number) {
+    return {
+      message: `Customer id ${adminId}`,
+      data: this.adminService.findOne(adminId),
+    };
+  }
+
+  @Post()
+  create(@Body() payload: CreateAdminDto) {
+    return {
+      message: `Customer created`,
+      data: this.adminService.create(payload),
+    };
+  }
+
+  @Put(':adminId')
+  update(
+    @Param('adminId', ParseIntPipe) adminId: number,
+    @Body() payload: UpdateAdminDto,
+  ) {
+    return {
+      message: `Customer ${adminId} updates`,
+      data: this.adminService.update(adminId, payload),
+    };
+  }
+
+  @Delete(':adminId')
+  remove(@Param('adminId', ParseIntPipe) adminId: number) {
+    return {
+      message: `Customer ${adminId} has been removed from DB`,
+      data: this.adminService.remove(adminId),
+    };
+  }
+}
--- src/users/services/admins/admins.service.ts
+++ src/users/services/admins/admins.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { Admin } from 'src/users/entities/admin.entity';
+
+import { CreateAdminDto, UpdateAdminDto } from 'src/users/dtos/admins.dtos';
 
 @Injectable()
-export class AdminsService {}
+export class AdminsService {
+  private counterIdentifier = 1;
+  private admins: Admin[] = [
+    {
+      identifier: 1,
+      password: '123456',
+      email: 'test@shop.com',
+      role: 'salesman',
+    },
+  ];
+
+  findOne(index: number) {
+    const admin = this.admins.find((item) => item.identifier === index);
+    if (!admin) {
+      throw new NotFoundException(
+        `There's no a admin assigned to ${index} identifier`,
+      );
+    } else {
+      return admin;
+    }
+  }
+
+  create(payload: CreateAdminDto) {
+    this.counterIdentifier += 1;
+    const admin = {
+      identifier: this.counterIdentifier,
+      ...payload,
+    };
+
+    this.admins.push(admin);
+
+    return admin;
+  }
+
+  update(identifier: number, payload: UpdateAdminDto) {
+    const admin = this.findOne(identifier);
+
+    if (admin) {
+      const index = this.admins.findIndex(
+        (item) => item.identifier === identifier,
+      );
+      this.admins[index] = {
+        ...admin,
+        ...payload,
+      };
+      return this.admins[index];
+    }
+  }
+
+  remove(identifier: number) {
+    const index = this.admins.findIndex(
+      (item) => item.identifier === identifier,
+    );
+    if (index === -1) {
+      throw new NotFoundException(
+        `There's no a admin assigned to ${index} identifier`,
+      );
+    }
+    this.admins.splice(index, 1);
+    return true;
+  }
+}
