d6867f9050be093daf7ca70bc2ebde67d2b08400
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-26 22:16:25 -0500

    Customer
    Adding customer features using customer controller and service

--- src/users/controllers/customers/customers.controller.ts
+++ src/users/controllers/customers/customers.controller.ts
@@ -1,11 +1,56 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Put,
+  Get,
+  Post,
+  Delete,
+  Body,
+  Param,
+  Controller,
+  ParseIntPipe,
+} from '@nestjs/common';
+
+import { CustomersService } from 'src/users/services/customers/customers.service';
+
+import {
+  CreateCustomerDto,
+  UpdateCustomerDto,
+} from 'src/users/dtos/customers.dtos';
 
 @Controller('customers')
 export class CustomersController {
+  constructor(private customerServive: CustomersService) {}
   @Get(':customerId')
-  getDetails(@Param('customerId') customerId: string) {
+  getDetails(@Param('customerId', ParseIntPipe) customerId: number) {
     return {
       message: `Customer id ${customerId}`,
+      data: this.customerServive.findOne(customerId),
+    };
+  }
+
+  @Post()
+  create(@Body() payload: CreateCustomerDto) {
+    return {
+      message: `Customer created`,
+      data: this.customerServive.create(payload),
+    };
+  }
+
+  @Put(':customerId')
+  update(
+    @Param('customerId', ParseIntPipe) customerId: number,
+    @Body() payload: UpdateCustomerDto,
+  ) {
+    return {
+      message: `Customer ${customerId} updates`,
+      data: this.customerServive.update(customerId, payload),
+    };
+  }
+
+  @Delete(':customerId')
+  remove(@Param('customerId', ParseIntPipe) customerId: number) {
+    return {
+      message: `Customer ${customerId} has been removed from DB`,
+      data: this.customerServive.remove(customerId),
     };
   }
 }
--- src/users/services/customers/customers.service.ts
+++ src/users/services/customers/customers.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { Customer } from 'src/users/entities/customer.entity';
+
+import {
+  CreateCustomerDto,
+  UpdateCustomerDto,
+} from 'src/users/dtos/customers.dtos';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  private counterIdentifier = 1;
+  private customers: Customer[] = [
+    {
+      identifier: 1,
+      name: 'Test',
+      lastName: 'Man',
+      phone: '3123456789',
+    },
+  ];
+
+  findOne(index: number) {
+    const order = this.customers.find((item) => item.identifier === index);
+    if (!order) {
+      throw new NotFoundException(
+        `There's no a order assigned to ${index} identifier`,
+      );
+    } else {
+      return order;
+    }
+  }
+
+  create(payload: CreateCustomerDto) {
+    this.counterIdentifier += 1;
+    const newProduct = {
+      identifier: this.counterIdentifier,
+      ...payload,
+    };
+
+    this.customers.push(newProduct);
+
+    return newProduct;
+  }
+
+  update(identifier: number, payload: UpdateCustomerDto) {
+    const order = this.findOne(identifier);
+
+    if (order) {
+      const index = this.customers.findIndex(
+        (item) => item.identifier === identifier,
+      );
+      this.customers[index] = {
+        ...order,
+        ...payload,
+      };
+      return this.customers[index];
+    }
+  }
+
+  remove(identifier: number) {
+    const index = this.customers.findIndex(
+      (item) => item.identifier === identifier,
+    );
+    if (index === -1) {
+      throw new NotFoundException(
+        `There's no a order assigned to ${index} identifier`,
+      );
+    }
+    this.customers.splice(index, 1);
+    return true;
+  }
+}
