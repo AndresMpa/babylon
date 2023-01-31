7f2757de5f8d1b6552ac1aa61e5a2141194eb3d5
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-31 17:33:35 -0500

    CORS
    Enabling CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 const stage = process.env.NODE_ENV === 'production';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   // Global pipes
   app.useGlobalPipes(
@@ -30,6 +30,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
