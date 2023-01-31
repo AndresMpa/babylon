c57a98314ccd9c638c8be6368f282a08ece5b874
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-31 08:09:18 -0500

    Global injection
    Using global injection, also adding an example using the app stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+
+import { AppService } from './app.service';
 
 import { StockModule } from './stock/stock.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 
+import { environments } from './environments';
+
 @Module({
-  imports: [StockModule, UsersModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    StockModule,
+    UsersModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[],
+    private configService: ConfigService,
+  ) {}
+
+  getData() {
+    const tasks = this.configService.get<string>('TASK_API');
+    const apiKey = this.configService.get<string>('API_KEY');
+
+    console.log(tasks, apiKey, this.apiKey, this.tasks);
+
+    return {
+      tasks,
+      apiKey,
+      'injected apiKey': this.apiKey,
+      'injected task': this.tasks,
+    };
   }
 }
