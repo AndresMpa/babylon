32db2861b4f6ae900eb4b75251f9f948bf465e03
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-31 17:09:56 -0500

    Setting up Swagger
    Adding Swagger to main module, using default (Nothing fancy) recommended
    configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 const stage = process.env.NODE_ENV === 'production';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global pipes
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // To ignore data that isn't in the DTOs
@@ -13,6 +17,19 @@ async function bootstrap() {
       disableErrorMessages: stage, //To disable error message
     }),
   );
+
+  // Documentation configuration
+  const config = new DocumentBuilder()
+    .setTitle('Documentation')
+    .setDescription('An example of API description')
+    .setVersion('1.0')
+    .addTag('doc')
+    .build();
+
+  // Documentation instance
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
