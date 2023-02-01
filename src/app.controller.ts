b5687d3bfe9d2ef057ae426534d4882c90cada44
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-01 01:20:37 -0500

    Clean up
    Removing old examples to create new ones with postgres

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,19 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /**
+    Returns a simple example
+  */
   @Get()
   getExample() {
     return this.appService.getData();
+  }
+
+  /**
+    Returns tasks from postgres
+  */
+  @Get('task')
+  getTask() {
+    return this.appService.getTask();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { Client } from 'pg';
 
 import { ConfigService, ConfigType } from '@nestjs/config';
 
@@ -7,8 +8,7 @@ import config from './config';
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('API_KEY') private apiKey: string,
-    @Inject('TASKS') private tasks: any[],
+    @Inject('POSTGRES') private postgresInstance: Client,
 
     @Inject(config.KEY) private configAsType: ConfigType<typeof config>,
 
@@ -19,25 +19,26 @@ export class AppService {
     const tasksFromInjection = this.configService.get<string>('TASK_API');
     const apiKeyFromInjection = this.configService.get<string>('API_KEY');
 
-    const apiKeyFromType = this.configAsType.apiKey;
     const dataBase = this.configAsType.database;
 
-    console.log(
-      this.apiKey,
-      this.tasks,
-      tasksFromInjection,
-      apiKeyFromInjection,
-      dataBase,
-      apiKeyFromType,
-    );
+    console.log(tasksFromInjection, apiKeyFromInjection, dataBase);
 
     return {
-      'injected apiKey': this.apiKey,
-      'injected task': this.tasks,
       tasksFromInjection,
       apiKeyFromInjection,
       dataBase,
-      apiKeyFromType,
     };
+  }
+
+  getTask() {
+    return new Promise((resolve, reject) => {
+      this.postgresInstance.query('SELECT * FROM tasks', (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response.rows);
+        }
+      });
+    });
   }
 }
