58a13ff3c613562947388f95af5cfe31af052553
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-01 01:17:52 -0500

    Updating config
    Using postgres from configuration file (config.ts)

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,12 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('config', () => {
   return {
     database: {
-      name: process.env.DATABASE_NAME,
-      port: process.env.DATABASE_PORT,
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      password: process.env.DATABASE_PASSWORD,
+      user: process.env.DATABASE_USER,
+      uri: process.env.DATABASE_URI,
     },
-    apiKey: process.env.API_KEY,
   };
 });
