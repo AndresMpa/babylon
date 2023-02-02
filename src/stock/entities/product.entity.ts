9f4560a0542b11189b75cad0158f46f96250bec7
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-01 22:56:02 -0500

    Product entity
    Using the product entity as an example, also first changed entity to a
    SQL entities model

--- src/stock/entities/product.entity.ts
+++ src/stock/entities/product.entity.ts
@@ -1,8 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   identifier: number;
+
+  @Column({
+    unique: true,
+    type: 'varchar',
+    length: 255,
+  })
   name: string;
+
+  @Column({
+    type: 'text'
+  })
   description: string;
+
+  @Column({
+    type: 'int'
+  })
   price: number;
+
+  @Column({
+    type: 'int'
+  })
   stock: number;
+
+  @Column({
+    type: 'varchar'
+  })
   image: string;
 }
