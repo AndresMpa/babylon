ae9309fcf1f1e17f06839e35d1bf4aa78305a8d8
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-04 01:17:12 -0500

    CLI requirements
    Using a dataSource.ts file to handle with TypeORM DataSource object
    required for DB connection

---
+++ src/database/dataSource.ts
@@ -0,0 +1,24 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+
+const connectionSource = new DataSource({
+  type: 'postgres',
+  port: parseInt(configService.get('DATABASE_PORT'), 10),
+  host: configService.get('DATABASE_HOST'),
+  username: configService.get('DATABASE_USER'),
+  password: configService.get('DATABASE_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
+  logging: configService.get('ORM_LOG'),
+  synchronize: configService.get('ORM_SYNC'),
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+});
+
+connectionSource.initialize();
+
+export { connectionSource };
