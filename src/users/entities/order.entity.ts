a19325c3d364afe522f19ae02bb522d481b78f03
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-04 01:16:01 -0500

    TypeORM CLI
    Adding scripts to package.js file to handle with TypeORM CLI

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -1,13 +1,7 @@
-import { Column, Entity } from 'typeorm';
-
-import { Customer } from './customer.entity';
 import { Product } from 'src/stock/entities/product.entity';
+import { Customer } from './customer.entity';
 
-@Entity({ name: 'orders' })
 export class Order {
-  @Column({
-    type: 'date',
-  })
   date: Date;
   owner: Customer;
   products: Product[];
