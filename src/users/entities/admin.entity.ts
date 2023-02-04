ba1a59c255eac3bd4eba022aac7372c67181d335
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-04 00:29:35 -0500

    Quick fix
    Removing capitalized cases

--- src/users/entities/admin.entity.ts
+++ src/users/entities/admin.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: 'Admins' })
+@Entity({ name: 'admins' })
 export class Admin {
   @PrimaryGeneratedColumn()
   identifier: number;
