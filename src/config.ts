479e961d0eb52fffdcd487d1b24bf97593e6f100
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-04 00:28:45 -0500

    ORM
    Adding some data for ORM configuration to general configuration file

--- src/config.ts
+++ src/config.ts
@@ -3,12 +3,17 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('config', () => {
   return {
     database: {
+      type: process.env.DATABASE_TYPE,
       host: process.env.DATABASE_HOST,
       database: process.env.DATABASE_NAME,
       port: parseInt(process.env.DATABASE_PORT, 10),
       password: process.env.DATABASE_PASSWORD,
       user: process.env.DATABASE_USER,
       uri: process.env.DATABASE_URI,
+    },
+    ormConfig: {
+      logging: process.env.ORM_LOG === 'true',
+      synchronize: process.env.ORM_SYNC === 'true',
     },
   };
 });
