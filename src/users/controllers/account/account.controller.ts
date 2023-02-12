8a56cc83c982114fcad973ca5f4c4593be8e9b8b
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-11 20:01:22 -0500

    HTTP basics
    Adding basic endpoints

--- src/users/controllers/account/account.controller.ts
+++ src/users/controllers/account/account.controller.ts
@@ -1,4 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  Get,
+  Put,
+  Post,
+  Delete,
+  Body,
+  Param,
+  Controller,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateAccountDto, UpdateAccountDto } from 'src/users/dtos/account.dto';
 
-@Controller('account')
-export class AccountController {}
+import { AccountService } from 'src/users/services/account/account.service';
+
+@ApiTags('Customers')
+@Controller('accounts')
+export class AccountController {
+  constructor(private accountService: AccountService) {}
+
+  /**
+    Returns all accounts information
+  */
+  @Get()
+  findAll() {
+    return this.accountService.findAll();
+  }
+
+  /**
+    Returns specific information from an account  using its identifier
+  */
+  @Get(':accountId')
+  getDetails(@Param('accountId', ParseIntPipe) accountId: number) {
+    return this.accountService.findOne(accountId);
+  }
+
+  /**
+    Creates an account using a payload
+  */
+  @Post()
+  create(@Body() payload: CreateAccountDto) {
+    return this.accountService.create(payload);
+  }
+
+  /**
+    Updates an account using its identifier and a payload
+  */
+  @Put(':accountId')
+  update(
+    @Param('accountId') accountId: number,
+    @Body() payload: UpdateAccountDto,
+  ) {
+    return this.accountService.update(accountId, payload);
+  }
+
+  /**
+    Deletes an account using its identifier
+  */
+  @Delete(':accountId')
+  remove(@Param('accountId') accountId: number) {
+    return this.accountService.remove(accountId);
+  }
+}
