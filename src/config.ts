3e3d77b7ca236eaccbb6fc0cf9177f535993faa4
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-24 01:00:21 -0500

    Env vars
    Tracking environmental variables as DB parameters

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,12 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('config', () => {
   return {
     database: {
-      name: process.env.DATABASE_NAME,
-      port: process.env.DATABASE_PORT,
+      connection: process.env.DATABASE_CONNECTION,
+      password: process.env.DATABASE_PASSWORD,
+      user: process.env.DATABASE_USER,
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      databaseName: process.env.DATABASE_NAME,
     },
-    apiKey: process.env.API_KEY,
   };
 });
