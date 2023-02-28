b3329a69b5708a31e3157a0a2ce20fa277317865
Andres M. Prieto <andresprieto2703@gmail.com>
2023-02-28 13:51:07 -0500

    1:1
    Using a 1:1 raw relation on products to handle with categories

--- src/stock/entities/product.entity.ts
+++ src/stock/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory, raw } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @Schema({
@@ -19,8 +19,21 @@ export class Product extends Document {
 
   @Prop()
   image: string;
+
+  @Prop(
+    raw({
+      name: {
+        type: String,
+      },
+
+      description: {
+        type: String,
+      },
+    }),
+  )
+  category: Record<string, any>;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
 
-ProductSchema.index({ price: 1, stock: -1 })
+ProductSchema.index({ price: 1, stock: -1 });
