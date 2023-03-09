7b6f4c6c7c557c140ee5b88aca4ae5bab4c6fa17
Andres M. Prieto <andresprieto2703@gmail.com>
2023-03-08 19:52:48 -0500

    Fixing dependencies
    At user module there was dependency that it wasn't working, I added it

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,19 +1,36 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 
 // Controllers
-import { AdminsController } from './controllers/admins/admins.controller';
+import { AccountsController } from './controllers/accounts/accounts.controller';
 import { CustomersController } from './controllers/customers/customers.controller';
 
 // Services
-import { AdminsService } from './services/admins/admins.service';
+import { AccountsService } from './services/accounts/accounts.service';
 import { CustomersService } from './services/customers/customers.service';
+
+// Schemes
+import { Account, AccountSchema } from './entities/account.entity';
+import { Customer, CustomerSchema } from './entities/customer.entity';
 
 // Modules
 import { StockModule } from 'src/stock/stock.module';
 
 @Module({
-  controllers: [CustomersController, AdminsController],
-  providers: [CustomersService, AdminsService],
-  imports: [StockModule],
+  imports: [
+    StockModule,
+    MongooseModule.forFeature([
+      {
+        name: Account.name,
+        schema: AccountSchema,
+      },
+      {
+        name: Customer.name,
+        schema: CustomerSchema,
+      },
+    ]),
+  ],
+  controllers: [CustomersController, AccountsController],
+  providers: [CustomersService, AccountsService],
 })
 export class UsersModule {}
