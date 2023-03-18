6f7f620984e0a6389826ce2437926c4d6b6aba0e
Andres M. Prieto <andresprieto2703@gmail.com>
2023-03-18 10:03:58 -0500

    Using some subentities
    Using classes to handle with relations

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,6 +1,23 @@
-export class Customer {
-  identifier: number;
-  lastName: string;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+import { SubPreference, SubPreferenceSchema } from './subPreference.entity';
+
+@Schema()
+export class Customer extends Document {
+  @Prop({ required: true })
   name: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop()
   phone: string;
+
+  @Prop({
+    type: [SubPreferenceSchema],
+  })
+  preferences: Types.Array<SubPreference>;
 }
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer);
--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -1,8 +1,15 @@
-import { Product } from 'src/stock/entities/product.entity';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
 import { Customer } from './customer.entity';
 
-export class Order {
+@Schema()
+export class Order extends Document {
+  @Prop({ type: Date })
   date: Date;
-  owner: Customer;
-  products: Product[];
+
+  @Prop({ type: Types.ObjectId, ref: Customer.name, required: true })
+  customer: Customer | Types.ObjectId;
 }
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
