40321bc43a5250743ceb771f45606515998b32e0
Andres M. Prieto <andresprieto2703@gmail.com>
2023-03-21 13:31:35 -0500

    Encryption
    Encrypting passwords on DB, also adding find by email option

--- src/users/services/accounts/accounts.service.ts
+++ src/users/services/accounts/accounts.service.ts
@@ -2,8 +2,12 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import * as bcrypt from 'bcrypt';
+
 import { Account } from 'src/users/entities/account.entity';
+
 import { CreateAccountDto, UpdateAccountDto } from 'src/users/dtos/account.dto';
+
 import { CustomersService } from '../customers/customers.service';
 
 @Injectable()
@@ -16,6 +20,7 @@ export class AccountsService {
 
   async findAll() {
     return await this.accountRepository.find({
+      select: ['email', 'role'],
       relations: ['customer'],
     });
   }
@@ -31,8 +36,14 @@ export class AccountsService {
     }
   }
 
+  async findByEmail(email: string) {
+    return await this.accountRepository.find({ where: { email } });
+  }
+
   async create(payload: CreateAccountDto) {
     const newAccount = await this.accountRepository.create(payload);
+    const hashPassword = await bcrypt.hash(newAccount.password, 10);
+    newAccount.password = hashPassword;
     if (payload.customerIdentifier) {
       const customer = await this.customersService.findOne(
         payload.customerIdentifier,
