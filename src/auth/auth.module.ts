9f73411e5243663d323f5077e78f7b7eacd2cce3
Andres M. Prieto <andresprieto2703@gmail.com>
2023-03-23 10:44:01 -0500

    Controller
    Adding auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
 
-// Injectables
+// Modules
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 
+// Services
 import { AuthenticationService } from './services/authentication/authentication.service';
-
 import { LocalStrategy } from './strategies/local.strategy';
+
+// Controllers
+import { AuthenticationController } from './controllers/authentication/authentication.controller';
 
 @Module({
   imports: [UsersModule, PassportModule],
+  controllers: [AuthenticationController],
   providers: [AuthenticationService, LocalStrategy],
 })
 export class AuthModule {}
