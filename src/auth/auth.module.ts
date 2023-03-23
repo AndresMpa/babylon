61f72976335e6d876bee3839b041e730a7f2ab49
Andres M. Prieto <andresprieto2703@gmail.com>
2023-03-23 00:20:25 -0500

    Auth module
    Adding auth module configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
+// Injectables
+import { AccountsService } from 'src/users/services/accounts/accounts.service';
+
+import { AuthenticationService } from './services/authentication/authentication.service';
+
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [AccountsService, PassportModule],
+  providers: [AuthenticationService, LocalStrategy],
+})
 export class AuthModule {}
