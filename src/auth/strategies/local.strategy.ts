d82d3167e826533e202a0f2712337d28c6b89605
Andres M. Prieto <andresprieto2703@gmail.com>
2023-03-23 00:23:15 -0500

    Local strategy
    Adding local strategy to auth module

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+
+import { AuthenticationService } from '../services/authentication/authentication.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
+  constructor(private authService: AuthenticationService) {
+    super();
+  }
+
+  async validate(email: string, password: string) {
+    const user = await this.authService.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException('You are not allow to continue');
+    }
+    return user;
+  }
+}
