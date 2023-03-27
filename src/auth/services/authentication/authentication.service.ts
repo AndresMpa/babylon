947df5a921ad164c35d73b4ae73784ab858ca5a2
Andres M. Prieto <andresprieto2703@gmail.com>
2023-03-26 20:20:41 -0500

    JWT
    Just adding JWT service to authentication service

--- src/auth/services/authentication/authentication.service.ts
+++ src/auth/services/authentication/authentication.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 import { AccountsService } from 'src/users/services/accounts/accounts.service';
+import { Account } from 'src/users/entities/account.entity';
+import { PayloadToken } from 'src/auth/model/token.model';
+
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private accountService: AccountsService) {}
+  constructor(
+    private accountService: AccountsService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.accountService.findByEmail(email);
@@ -17,5 +24,13 @@ export class AuthenticationService {
     } else {
       return null;
     }
+  }
+
+  generateJWT(user: Account) {
+    const payload: PayloadToken = { role: user.role, sub: user.identifier };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user,
+    };
   }
 }
