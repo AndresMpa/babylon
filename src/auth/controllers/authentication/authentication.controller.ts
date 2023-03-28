a43af630a2b1b68ccaf061b9758b770df9d22328
Andres M. Prieto <andresprieto2703@gmail.com>
2023-03-28 13:49:56 -0500

    Generating JWT
    Using a login endpoint to handle with JWT generation

--- src/auth/controllers/authentication/authentication.controller.ts
+++ src/auth/controllers/authentication/authentication.controller.ts
@@ -3,19 +3,27 @@ import { AuthGuard } from '@nestjs/passport';
 import { ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 
+// Authentication service
+import { AuthenticationService } from 'src/auth/services/authentication/authentication.service';
+
+// Account entity
+import { Account } from 'src/users/entities/account.entity';
+
 @ApiTags('Authentication')
 @Controller('authentication')
 @UseGuards(AuthGuard('local'))
 export class AuthenticationController {
+  constructor(private authService: AuthenticationService) {}
   /**
     Allow user to log in to the API
     @example: {
       "email": "mail@to.com",
-      "password": "something"
+      "role": "test"
     }
   */
   @Post('login')
   login(@Req() req: Request) {
-    return req.user;
+    const user = req.user as Account;
+    return this.authService.generateJWT(user);
   }
 }
