d88206ef493c0fb87129f9e3a9a482383cca90af
Andres M. Prieto <andresprieto2703@gmail.com>
2023-08-31 16:17:22 -0500

    Testing json
    Adding some response on Json format

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import { Request, Response } from "express";
 import * as express from "express";
 
 import { config } from "./config/vars";
+import { initialState } from "./config/initialState";
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Typescript')
-});
+app.get("/", (req: Request, res: Response) => res.send("Hello Typescript"));
+
+app.get(`/api/${config.version}`, (req: Request, res: Response) =>
+  res.json(initialState),
+);
 
 app.listen(config.port, () => {
   console.log(`App listening at http://localhost:${config.port}`);
