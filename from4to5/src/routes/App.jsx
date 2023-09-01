ec0fc30d91622856b547fbf2fbd5d837e82cb417
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-01 14:43:46 -0500

    Some refactor
    Moving things around to handle with React changes

--- from4to5/src/routes/App.jsx
+++ from4to5/src/routes/App.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../containers/Home';
-import Checkout from '../containers/Checkout';
-import Layout from '../components/Layout';
-import NotFound from '../containers/NotFound';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route component={NotFound} />
-      </Routes>
-    </Layout>
-  </BrowserRouter>
-);
+import Home from "../containers/Home";
+import Layout from "../components/Layout";
+import Checkout from "../containers/Checkout";
+import NotFound from "../containers/NotFound";
+
+import AppContext from "../context/AppContext";
+import useInitialState from "../hooks/useInitialState";
+
+const App = () => {
+  const initialState = useInitialState();
+  const isEmpty = Object.keys(initialState.products).length;
+  return (
+    <>
+      {isEmpty > 0 ? (
+        <AppContext.Provider value={AppContext}>
+          <BrowserRouter>
+            <Layout>
+              <Routes>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/checkout" component={Checkout} />
+                <Route component={NotFound} />
+              </Routes>
+            </Layout>
+          </BrowserRouter>
+        </AppContext.Provider>
+      ) : (
+        <h1>Loading</h1>
+      )}
+    </>
+  );
+};
 
 export default App;
