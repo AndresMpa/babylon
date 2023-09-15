638c622d9843299bfcf91759bb6504d92df64cc9
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-15 11:34:43 -0500

    Theme
    Splitting components to compound also adding theme switcher

--- renderPropsAndRenderFunctions/src/Header/components/Header.jsx
+++ renderPropsAndRenderFunctions/src/Header/components/Header.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 
+import Icon from "@header/components/Icon";
+
+import moonIcon from "@assets/images/icon-moon.svg";
 import sunIcon from "@assets/images/icon-sun.svg";
 
 import "@styles/components/Header.styl";
 
+import { useTheme } from "../hooks/useTheme";
+import Title from "./Title";
+import Wrapper from "../containers/Wrapper";
+
 const Header = () => {
+  const { currentTheme, switchTheme } = useTheme();
+  const iconConfig = {
+    class: {
+      wrapper: "header--icon",
+      image: "header--icon__image",
+    },
+    icon: sunIcon,
+    alt: "Switch theme",
+  };
+
+  currentTheme === "dark"
+    ? (iconConfig.icon = sunIcon)
+    : (iconConfig.icon = moonIcon);
+
   return (
-    <section className="header">
-      <article className="header--wrapper">
-        <h1 className="header--title">todo</h1>
-        <figure className="header--icon">
-          <img
-            className="header--icon__image"
-            id="themeSwitcher"
-            src={sunIcon}
-            alt="alt"
-          />
-        </figure>
-      </article>
-    </section>
+    <Wrapper section="header" wrapper="header--wrapper">
+      <Title class="header--title" title="todo" />
+      <Icon config={iconConfig} clickHandler={switchTheme} />
+    </Wrapper>
   );
 };
 
