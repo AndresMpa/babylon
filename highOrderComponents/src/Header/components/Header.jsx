c2c8f68ab9ff5226a59281615db8c7befec9913c
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-16 17:18:29 -0500

    HOC
    Adding a HOC example for header

--- highOrderComponents/src/Header/components/Header.jsx
+++ highOrderComponents/src/Header/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 import Icon from "@header/components/Icon";
+import Title from "@header/components/Title";
+
+import Wrapper from "@header/containers/Wrapper";
 
 import moonIcon from "@assets/images/icon-moon.svg";
 import sunIcon from "@assets/images/icon-sun.svg";
@@ -8,10 +11,8 @@ import sunIcon from "@assets/images/icon-sun.svg";
 import "@styles/components/Header.styl";
 
 import { useTheme } from "../hooks/useTheme";
-import Title from "./Title";
-import Wrapper from "../containers/Wrapper";
 
-const Header = () => {
+const Header = (props) => {
   const { currentTheme, switchTheme } = useTheme();
   const iconConfig = {
     class: {
@@ -30,7 +31,7 @@ const Header = () => {
     <Wrapper
       section="header"
       wrapper="header--wrapper"
-      renderTitle={() => <Title class="header--title" title="todo" />}
+      renderTitle={() => <Title class="header--title" title={props.title} />}
       renderIcon={() => <Icon config={iconConfig} clickHandler={switchTheme} />}
     />
   );
--- highOrderComponents/src/Header/header.js
+++ highOrderComponents/src/Header/header.js
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 
 import Header from "@header/components/Header";
 
+import { withTitle } from "@header/HOC/withTitle";
+
 import "@styles/components/Basic.styl";
 
 const container = document.querySelector("#header");
 const root = createRoot(container);
 
-root.render(<Header />);
+const HeaderWithTitle = withTitle(Header)("Hey there, Welcome to!");
+
+root.render(<HeaderWithTitle title="todo" />);
