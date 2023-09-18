599e34b4a7e403045d1701c84e16be02c6701e73
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-18 02:40:28 -0500

    useEffect
    Understanding useEffect in deep

--- professionalStateManagementWithReact/src/components/VerificationState.jsx
+++ professionalStateManagementWithReact/src/components/VerificationState.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "@styles/components/verification.scss";
 
 const VerificationState = (props) => {
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  useEffect(() => {
+    // Fake backend behaviour
+    if (loading) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    }
+  }, [loading]);
 
   return (
     <article className="verification">
@@ -11,10 +21,12 @@ const VerificationState = (props) => {
       <p className="verification--text">{props.text}</p>
 
       {error && (
-        <p className="verification--text" onClick={() => useState(!error)}>
+        <p className="verification--text" onClick={() => setError(!error)}>
           Ups... There's an error, click here, please
         </p>
       )}
+
+      {loading && <p className="verification--text">Loading...</p>}
 
       <div className="verification--wrapper">
         <input
@@ -23,7 +35,11 @@ const VerificationState = (props) => {
           name={props.name}
           type="text"
         />
-        <button className="verification--button" disabled={error}>
+        <button
+          className="verification--button"
+          disabled={error}
+          onClick={() => setLoading(!loading)}
+        >
           {props.button}
         </button>
       </div>
