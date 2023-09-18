919abb70c010f20d6e093c2aa30479cc48910f74
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-17 23:26:57 -0500

    Webpack
    Extending webpack configs

---
+++ professionalStateManagementWithReact/webpack.config.dev.js
@@ -0,0 +1,96 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: {
+    // Add more entry point for code splitting
+    home: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    chunkFilename: "[name].bundle.js",
+    filename: "[name].bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      // Replace them if using splitting options
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@containers": path.resolve(__dirname, "src/containers/"),
+      "@context": path.resolve(__dirname, "src/context/"),
+      "@hooks": path.resolve(__dirname, "src/hooks/"),
+      "@routes": path.resolve(__dirname, "src/routes/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@assets": path.resolve(__dirname, "src/assets/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|gif|jpg|jpeg|svg)$/,
+        type: "asset",
+      },
+    ],
+  },
+  devServer: {
+    watchFiles: ["./src/**/*", "./public/**/*"],
+    liveReload: true,
+    compress: true,
+    hot: true,
+    port: process.env.PORT,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    minimize: true,
+    minimizer: [
+      new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "assets/[name].css",
+      }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              ["svgo", { plugins: [{ name: "preset-default" }] }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+};
