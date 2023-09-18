8a8c2ec9380dac11b1b4784de625625515f8c0fa
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-18 01:58:45 -0500

    Linter
    Just using prettier

--- professionalStateManagementWithReact/webpack.config.dev.js
+++ professionalStateManagementWithReact/webpack.config.dev.js
@@ -1,29 +1,29 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
   entry: {
     // Add more entry point for code splitting
-    home: "./src/index.js",
+    home: './src/index.js',
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    chunkFilename: "[name].bundle.js",
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: '[name].bundle.js',
+    filename: '[name].bundle.js',
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
       // Replace them if using splitting options
-      "@components": path.resolve(__dirname, "src/components/"),
-      "@containers": path.resolve(__dirname, "src/containers/"),
-      "@context": path.resolve(__dirname, "src/context/"),
-      "@hooks": path.resolve(__dirname, "src/hooks/"),
-      "@routes": path.resolve(__dirname, "src/routes/"),
-      "@styles": path.resolve(__dirname, "src/styles/"),
-      "@assets": path.resolve(__dirname, "src/assets/"),
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@containers': path.resolve(__dirname, 'src/containers/'),
+      '@context': path.resolve(__dirname, 'src/context/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@routes': path.resolve(__dirname, 'src/routes/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@assets': path.resolve(__dirname, 'src/assets/'),
     },
   },
   module: {
@@ -32,34 +32,34 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: "/node_modules/",
+        use: 'ts-loader',
+        exclude: '/node_modules/',
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
           },
         ],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|gif|jpg|jpeg|svg)$/,
-        type: "asset",
+        type: 'asset',
       },
     ],
   },
   devServer: {
-    watchFiles: ["./src/**/*", "./public/**/*"],
+    watchFiles: ['./src/**/*', './public/**/*'],
     liveReload: true,
     compress: true,
     hot: true,
@@ -67,26 +67,27 @@ module.exports = {
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
     minimize: true,
     minimizer: [
       new HtmlWebPackPlugin({
-        template: "./public/index.html",
-        filename: "./index.html",
+        favicon: path.resolve(__dirname, 'src/assets/images/favicon.ico'),
+        template: path.resolve(__dirname, 'public/index.html'),
+        filename: './index.html',
       }),
       new MiniCssExtractPlugin({
-        filename: "assets/[name].css",
+        filename: 'assets/[name].css',
       }),
       new ImageMinimizerPlugin({
         minimizer: {
           implementation: ImageMinimizerPlugin.imageminMinify,
           options: {
             plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-              ["svgo", { plugins: [{ name: "preset-default" }] }],
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              ['svgo', { plugins: [{ name: 'preset-default' }] }],
             ],
           },
         },
