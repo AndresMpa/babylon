eb161d20d01bab8d495b4a18957155ffe01939dd
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-18 02:19:19 -0500

    Comparison
    Doing the same management using useState and Class

--- professionalStateManagementWithReact/src/components/VerificationClass.jsx
+++ professionalStateManagementWithReact/src/components/VerificationClass.jsx
@@ -3,11 +3,32 @@ import React, { Component } from "react";
 import "@styles/components/verification.scss";
 
 class VerificationClass extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: false,
+    };
+  }
   render() {
     return (
       <article className="verification">
         <h2 className="verification--title">{this.props.title}</h2>
         <p className="verification--text">{this.props.text}</p>
+
+        {this.state.error && (
+          <p
+            className="verification--text"
+            onClick={() =>
+              this.setState((previousState) => ({
+                error: !previousState.error,
+              }))
+            }
+          >
+            Ups... There's an error, click here, please
+          </p>
+        )}
+
         <div className="verification--wrapper">
           <input
             className="verification--input"
@@ -15,7 +36,9 @@ class VerificationClass extends Component {
             name={this.props.name}
             type="text"
           />
-          <button className="verification--button">{this.props.button}</button>
+          <button className="verification--button" disabled={this.state.error}>
+            {this.props.button}
+          </button>
         </div>
       </article>
     );
--- professionalStateManagementWithReact/src/components/VerificationState.jsx
+++ professionalStateManagementWithReact/src/components/VerificationState.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "@styles/components/verification.scss";
 
 const VerificationState = (props) => {
+  const [error, setError] = useState(false);
+
   return (
     <article className="verification">
       <h2 className="verification--title">{props.title}</h2>
       <p className="verification--text">{props.text}</p>
+
+      {error && (
+        <p className="verification--text" onClick={() => useState(!error)}>
+          Ups... There's an error, click here, please
+        </p>
+      )}
+
       <div className="verification--wrapper">
         <input
           className="verification--input"
@@ -14,7 +23,9 @@ const VerificationState = (props) => {
           name={props.name}
           type="text"
         />
-        <button className="verification--button">{props.button}</button>
+        <button className="verification--button" disabled={error}>
+          {props.button}
+        </button>
       </div>
     </article>
   );
