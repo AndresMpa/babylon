33273189e3288a96c3e4cc9cf88dd2441c127e66
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-20 08:02:56 -0500

    Linter
    Just using a linter

--- navigationWithReactRouter/webpack.config.prod.js
+++ navigationWithReactRouter/webpack.config.prod.js
@@ -1,24 +1,24 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CSSMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     // Add more entry point for code splitting
-    home: "./src/index.js",
+    home: './src/index.js',
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    chunkFilename: "[name].bundle.js",
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: '[name].bundle.js',
+    filename: '[name].bundle.js',
     clean: true,
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
       // Replace them if using splitting options
       '@components': path.resolve(__dirname, 'src/components/'),
@@ -39,29 +39,29 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: "/node_modules/",
+        use: 'ts-loader',
+        exclude: '/node_modules/',
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
           },
         ],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|gif|jpg|jpeg|svg)$/,
-        type: "asset",
+        type: 'asset',
       },
     ],
   },
@@ -70,46 +70,46 @@ module.exports = {
     minimizer: [
       new TerserPlugin(),
       new HtmlWebPackPlugin({
-        favicon: "src/assets/images/favicon.ico",
-        template: "./public/index.html",
-        filename: "./index.html",
+        favicon: 'src/assets/images/favicon.ico',
+        template: './public/index.html',
+        filename: './index.html',
       }),
       new CSSMinimizerPlugin({
-        filename: "assets/[name].css",
+        filename: 'assets/[name].css',
       }),
       new ImageMinimizerPlugin({
         minimizer: {
           implementation: ImageMinimizerPlugin.imageminMinify,
           options: {
             plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-              ["svgo", { plugins: [{ name: "preset-default" }] }],
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              ['svgo', { plugins: [{ name: 'preset-default' }] }],
             ],
           },
         },
       }),
       new CleanWebpackPlugin(),
     ],
-    chunks: "all",
+    chunks: 'all',
     splitChunks: {
       cacheGroups: {
         default: false,
         commons: {
           test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          chunks: "all",
-          name: "commons",
-          filename: "assets/common.[chunkhash].js",
+          chunks: 'all',
+          name: 'commons',
+          filename: 'assets/common.[chunkhash].js',
           reuseExistingChunk: true,
           enforce: true,
           priority: 20,
         },
         vendors: {
           test: /[\\/]node_modules[\\/]/,
-          chunks: "all",
-          name: "vendors",
-          filename: "assets/vendor.[chunkhash].js",
+          chunks: 'all',
+          name: 'vendors',
+          filename: 'assets/vendor.[chunkhash].js',
           reuseExistingChunk: true,
           enforce: true,
           priority: 10,
