0cf25d73f01b230d98eb0027f768b46a5dcea9eb
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-22 08:40:53 -0500

    Working profile
    Adding a simple working profile

--- navigationWithReactRouter/skeletonPractice/src/components/ProfileView.jsx
+++ navigationWithReactRouter/skeletonPractice/src/components/ProfileView.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { usersList } from "@util/blogRefs";
+import { useParams } from "react-router-dom";
 
 const ProfileView = () => {
+  const { slug } = useParams();
 
   return (
     <>
-      <p>{username}</p>
+      <p>{slug}</p>
     </>
   );
 };
