f945316d599a091513c56cfddf284a079db406ea
Andres M. Prieto <andresprieto2703@gmail.com>
2023-09-25 23:47:55 -0500

    Better 404
    Just adding a better 404

--- reactWithViteAndTailwindCSS/src/pages/NotFound/index.jsx
+++ reactWithViteAndTailwindCSS/src/pages/NotFound/index.jsx
@@ -1,5 +1,14 @@
+import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
+
 function NotFound() {
-  return <div className="bg-blue-600">Hey world</div>;
+  return (
+    <div className="flex flex-col items-center p-6">
+      <ExclamationTriangleIcon className="h-40 w-40 my-6 text-red-500" />
+
+      <h2 className="font-semibold text-2xl text-red-500 my-3">Ops... This pages was not found</h2>
+      <p>It seems that there is an error here, try going back using a simple</p>
+    </div>
+  );
 }
 
 export default NotFound;
