ae639f072d8cc9efe41fc70634ccbfee8bab8d60
Andres M. Prieto <andresprieto2703@gmail.com>
2023-10-05 03:00:56 -0500

    Legacy redux store
    Adding a simple example of how to use Redux legacy store

--- reactRedux/src/main.jsx
+++ reactRedux/src/main.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { Provider } from "react-redux";
+// This is the old way to do it
+import { legacy_createStore as createStore } from "redux";
+import { reducer } from "./reducers/index.jsx";
+
 import App from "./App.jsx";
 
 import "./style/index.css";
 
+const store = createStore(reducer);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
