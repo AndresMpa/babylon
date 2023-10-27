74663b746ad92fd6c63832b8cfd3820d6c55029e
Andres M. Prieto <andresprieto2703@gmail.com>
2023-10-27 00:18:38 -0500

    Redux Thunk update
    Moving variables inside components to handle with redux-thunk

--- reactRedux/src/components/PokeCard.jsx
+++ reactRedux/src/components/PokeCard.jsx
@@ -4,18 +4,12 @@ import { StarOutlined } from "@ant-design/icons";
 
 import "./../style/pokecard.css";
 
-const pokeImage = (pokemonIndex) =>
-  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
-
-const PokeCard = ({ pokemonInfo: { name, url } }) => {
-  const index = url.split("/")[6].split(".")[0];
-  const pokemonUrl = pokeImage(index);
-
+const PokeCard = ({ name, image }) => {
   return (
     <div className="pokecard">
       <Card
         title={name}
-        cover={<img src={pokemonUrl} alt={name} extra={<StarOutlined />} />}
+        cover={<img src={image} alt={name} extra={<StarOutlined />} />}
         extra={<StarOutlined />}
       >
         <Meta description={name}></Meta>
--- reactRedux/src/components/Pokedex.jsx
+++ reactRedux/src/components/Pokedex.jsx
@@ -4,7 +4,11 @@ import "./../style/pokedex.css";
 
 const Pokedex = ({ pokemons }) => {
   const pokeList = pokemons.map((pokemon, index) => (
-    <PokeCard key={index} pokemonInfo={pokemon} />
+    <PokeCard
+      key={pokemon.name}
+      name={pokemon.name}
+      image={pokemon.sprites.front_default}
+    />
   ));
 
   return <div className="pokedex">{pokeList}</div>;
