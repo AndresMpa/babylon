e9c209bfdc280b0e2ac31727940622779cd9ddd5
Andres M. Prieto <andresprieto2703@gmail.com>
2023-10-27 01:16:16 -0500

    Loader
    Adding spinner from ant design to have a loader under state manager

--- reactRedux/src/App.jsx
+++ reactRedux/src/App.jsx
@@ -1,21 +1,25 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { Col } from "antd";
+import { Col, Spin } from "antd";
 import Search from "./components/Search";
 import Pokedex from "./components/Pokedex";
 
 import { getPokemon } from "./api";
-import { getPokemonWithDetails, setPokemon } from "./actions";
+import { setPokemon, setLoading, getPokemonWithDetails } from "./actions";
 
 const App = () => {
   const pokemons = useSelector((state) => state.pokemons);
+  const loading = useSelector((state) => state.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemon = async () => {
+      dispatch(setLoading(true));
       const pokemonList = await getPokemon();
+
       dispatch(getPokemonWithDetails(pokemonList));
+      dispatch(setLoading(false));
     };
 
     fetchPokemon();
@@ -26,7 +30,13 @@ const App = () => {
       <Col span={12} offset={6}>
         <Search></Search>
       </Col>
-      <Pokedex pokemons={pokemons}></Pokedex>
+      {loading ? (
+        <Col offset={12}>
+          <Spin spinning size="large"></Spin>
+        </Col>
+      ) : (
+        <Pokedex pokemons={pokemons}></Pokedex>
+      )}
     </>
   );
 };
