2abc2bcb27e9ffe7c2ef33daba51d0faa27acf36
Andres M. Prieto <andresprieto2703@gmail.com>
2023-10-29 00:46:28 -0500

    Better card
    Adding description and skills for poke-card

--- reactRedux/src/components/PokeCard.jsx
+++ reactRedux/src/components/PokeCard.jsx
@@ -1,20 +1,79 @@
-import { Card } from "antd";
+import { useState } from "react";
+
 import Meta from "antd/es/card/Meta";
-import { StarOutlined } from "@ant-design/icons";
+import { Button, Card, Descriptions, Divider, Space } from "antd";
+import {
+  BookOutlined,
+  HeartOutlined,
+  SmallDashOutlined,
+  RadarChartOutlined,
+} from "@ant-design/icons";
 
 import "./../style/pokecard.css";
 
-const PokeCard = ({ name, image }) => {
+const PokeCard = ({
+  pokemonData: { name, image, types, description, information },
+}) => {
+  const [pokemonDescription, setPokemonDescription] = useState(false);
+  const [pokemonAbilities, setPokemonAbilities] = useState(false);
+
   return (
-    <div className="pokecard">
-      <Card
-        title={name}
-        cover={<img src={image} alt={name} extra={<StarOutlined />} />}
-        extra={<StarOutlined />}
-      >
-        <Meta description={name}></Meta>
-      </Card>
-    </div>
+    <Card
+      title={name}
+      cover={<img src={image} alt={name} extra={<HeartOutlined />} />}
+      extra={<HeartOutlined />}
+      actions={[
+        <RadarChartOutlined
+          key="description"
+          onClick={() => {
+            setPokemonDescription(true);
+            setPokemonAbilities(false);
+          }}
+        />,
+        <SmallDashOutlined
+          key="show"
+          onClick={() => {
+            setPokemonDescription(false);
+            setPokemonAbilities(false);
+          }}
+        />,
+        <BookOutlined
+          key="abilities"
+          onClick={() => {
+            setPokemonDescription(false);
+            setPokemonAbilities(true);
+          }}
+        />,
+      ]}
+    >
+      <Meta
+        description={
+          <div className="description">
+            {types.map((type, index) => (
+              <Button key={index} shape="round">
+                {type}
+              </Button>
+            ))}
+          </div>
+        }
+      ></Meta>
+      {pokemonDescription && (
+        <Descriptions
+          className="centerExtraData"
+          title={`${name}'s description`}
+          items={description}
+          layout="vertical"
+        />
+      )}
+      {pokemonAbilities && (
+        <Descriptions
+          className="centerExtraData"
+          title={`${name}'s information`}
+          items={information}
+          layout="vertical"
+        />
+      )}
+    </Card>
   );
 };
 
