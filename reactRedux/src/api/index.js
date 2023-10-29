5cbf4b8bc73d9a7752e53317bf64c18a8a7b54d0
Andres M. Prieto <andresprieto2703@gmail.com>
2023-10-29 18:19:41 -0500

    151/Favorite
    Reducing the amount of pokemon due to terrible dev exp

--- reactRedux/src/api/index.js
+++ reactRedux/src/api/index.js
@@ -4,14 +4,19 @@ const API = 'https://pokeapi.co/api/v2/pokemon';
 
 const getPokemon = () =>
   axios
-    .get(`${API}?limit=151&offset=0`)
+    .get(`${API}?limit=11&offset=0`) // Must be 151
     .then((res) => res.data.results)
     .catch((error) => console.log(error));
 
 const getDetails = (pokemon) =>
   axios
     .get(pokemon.url)
-    .then((res) => res.data)
+    .then((res) => {
+      return {
+        ...res.data,
+        favorite: false,
+      };
+    })
     .catch((error) => console.log(error));
 
 export { getPokemon, getDetails };
