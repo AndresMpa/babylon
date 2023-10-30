37d958040383e3ea9ed34e642d8927fcaab72f15
Andres M. Prieto <andresprieto2703@gmail.com>
2023-10-30 04:06:52 -0500

    Refactor
    Moving this into a better standard

--- reactRedux/src/main.jsx
+++ reactRedux/src/main.jsx
@@ -1,28 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import {
-  compose,
-  applyMiddleware,
-  legacy_createStore as createStore, // This is the old way to do it
-} from "redux";
-import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
-import { appReducer } from "./reducers";
-import { customizePokedex, logger } from "./middlewares";
-
-import App from "./App.jsx";
+import App from "./App";
 
 import "./style/index.css";
 
-const composeConfig = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const composedEnhancer = composeConfig(applyMiddleware(thunk, logger));
-
-const store = createStore(appReducer, composedEnhancer);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
