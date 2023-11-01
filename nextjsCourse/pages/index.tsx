d4a04c07e939e91c53e440ef104b39010f9f61a1
Andres M. Prieto <andresprieto2703@gmail.com>
2023-11-01 01:02:44 -0500

    Template repository
    Using a template to start with it

---
+++ nextjsCourse/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>Platzi and Next.js!</div>
+    </div>
+  )
+}
+
+export default HomePage
