ac7ef768f820b42a2973e2e84eee2dfca5f2267b
Andres M. Prieto <andresprieto2703@gmail.com>
2024-01-28 07:24:18 -0500

    Updating home

--- nextjsJamstack/pages/index.tsx
+++ nextjsJamstack/pages/index.tsx
@@ -1,71 +1,39 @@
-import { Grid } from '@ui/Grid'
-import { Button } from '@ui/Button'
-import { Typography } from '@ui/Typography'
-import { Layout } from '@components/Layout'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { getPlantList } from '@api/index'
 
-export default function Home() {
+import { PlantCollection } from '@components/PlantCollection'
+import { Authors } from '@components/Authors'
+import { Layout } from '@components/Layout'
+import { Hero } from '@components/Hero'
+
+type HomeProps = {
+  plantData: Plant[]
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const plantData = await getPlantList({ limit: 10 })
+
+  return {
+    props: { plantData },
+  }
+}
+
+export default function Home({
+  plantData,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout>
-      <Typography variant="h2" className="text-center">
-        👋 Curso de Next.js
-      </Typography>
-      <div className="max-w-5xl mx-auto my-10">
-        <Grid component="ul" container spacing={2}>
-          {documentationList.map((doc) => (
-            <Grid component="li" item className="" xs={6}>
-              <a
-                href={doc.link}
-                target="_blank"
-                title={doc.title}
-                className="p-4 border-2 border-gray-300 block hover:border-green-500 transition transition-colors duration-500"
-              >
-                <Typography variant="h5" className="mb-2">
-                  {doc.title}
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  {doc.description}
-                </Typography>
-              </a>
-            </Grid>
-          ))}
-        </Grid>
-      </div>
-      <footer className="text-center">
-        <Button
-          variant="outlined"
-          color="primary"
-          href="https://platzi.com/cursos/next-avanzado/"
-        >
-          🚀 Ir al curso
-        </Button>
-      </footer>
+      <Hero {...plantData[0]} className="mb-20" />
+      <Authors className="mb-10" />
+      <PlantCollection
+        plants={plantData.slice(1, 3)}
+        variant="vertical"
+        className="mb-24"
+      />
+      <PlantCollection
+        plants={plantData.lenght > 8 ? plantData.slice(3, 9) : plantData}
+        variant="square"
+      />
     </Layout>
   )
 }
-
-const documentationList = [
-  {
-    title: 'Documentación Proyecto',
-    description:
-      '¿Tienes dudas sobre este proyecto? Aquí encuentras la documentación para configurar todo. Aségurate de leerlo.',
-    link: 'https://github.com/jonalvarezz/platzi-nextjs-saga',
-  },
-  {
-    title: 'Documentación Next.js',
-    description:
-      'Aquí encuentras la documentación sobre el framework base con el que realizaremos todo.',
-    link: 'https://nextjs.org/docs/getting-started',
-  },
-  {
-    title: 'Documentación Contentful',
-    description:
-      'Nuestra aplicación conecta a Contenful para leer todo el contenido que mostraremos. Contenful provee la capa de GraphQL.',
-    link: 'https://www.contentful.com/developers/docs/references/content-delivery-api/',
-  },
-  {
-    title: 'Curso de Next.js Básico',
-    description:
-      '¿Olvidates algo sobre Next.js? En el curso básico puedes refrescar tu memoria y aprender los fundamentos.',
-    link: 'https://platzi.com/cursos/next/',
-  },
-]
