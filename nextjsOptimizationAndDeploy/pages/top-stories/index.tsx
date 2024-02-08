ee6ade83899ba47e75a797fb1efc0bad8e76f4d5
Andres M. Prieto <andresprieto2703@gmail.com>
2024-02-08 02:21:01 -0500

    Base page
    Adding base index for "top-stories"

---
+++ nextjsOptimizationAndDeploy/pages/top-stories/index.tsx
@@ -0,0 +1,45 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import Error from 'next/error'
+
+import { getAuthorList } from '@api'
+
+/**
+ * Redirects to /top-stories/<first-author-in-the-list>
+ */
+
+export const getServerSideProps: GetServerSideProps<{
+  error: number
+}> = async () => {
+  try {
+    const authors = await getAuthorList({ limit: 10 })
+
+    if (authors.length > 0) {
+      const firstAuthor = authors[0].handle
+
+      return {
+        redirect: {
+          destination: `/top-stories/${firstAuthor}`,
+          permanent: false,
+        },
+      }
+    }
+
+    return {
+      props: {
+        error: 412,
+      },
+    }
+  } catch (e) {
+    return {
+      props: {
+        error: 400,
+      },
+    }
+  }
+}
+
+export default function TopStoriesIndex({
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return <Error statusCode={error} />
+}
